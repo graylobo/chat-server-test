4219b19f6ae270a0dec6c36f13cb659e38598c35
HMC-MSI20220519\USER <sjmin@hmcnetworks.co.kr>
2022-06-13 11:29:51 +0900

    t

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
+// const app = require('express')();
+// const https = require('https').Server(app);
+// const io = require('socket.io')(https,{
+//   cors:{
+//     origin:"*"
+//   }
+// });
+// const moment = require('moment');
+// const port = process.env.PORT || 3000;
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+// app.get("/test",(req,res)=>{
+//   res.send("완료")
+// })
+// let totalCount =0;
+// io.on('connection', (socket) => {
+//   totalCount++;
+//   let name =""
+//   const dateFormat = "YYYY-MM-DD HH:mm:ss"
+//   socket.on("joinRoom",(userName)=>{
+//     name = userName;
+//     io.emit('chat message', `${userName}님이 입장했습니다. 현재 접속유저수:${totalCount}`);
+// })
+//   socket.on('chat message', msg => {
+//     io.emit('chat message', `${name}: ${msg} at ${moment(new Date()).format(dateFormat)}`);
+//   });
+
+//   socket.on("disconnect",()=>{
+//     totalCount--;
+//     io.emit('chat message', `${name}님이 나갔습니다. 현재 접속유저수:${totalCount}`);
+//   })
+// });
+
+// https.listen(port, () => {
+//   console.log(`Socket.IO server running at http://localhost:${port}/`);
+// });
+
+
+
+
 const app = require('express')();
-const https = require('https').Server(app);
-const io = require('socket.io')(https,{
-  cors:{
-    origin:"*"
-  }
-});
-const moment = require('moment');
-const port = process.env.PORT || 3000;
+app.listen(process.env.PORT||3000,()=>{
+  console.log("listening on...")
+})
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
 app.get("/test",(req,res)=>{
-  res.send("완료")
+  res.send("ttt")
 })
-let totalCount =0;
-io.on('connection', (socket) => {
-  totalCount++;
-  let name =""
-  const dateFormat = "YYYY-MM-DD HH:mm:ss"
-  socket.on("joinRoom",(userName)=>{
-    name = userName;
-    io.emit('chat message', `${userName}님이 입장했습니다. 현재 접속유저수:${totalCount}`);
-})
-  socket.on('chat message', msg => {
-    io.emit('chat message', `${name}: ${msg} at ${moment(new Date()).format(dateFormat)}`);
-  });
-
-  socket.on("disconnect",()=>{
-    totalCount--;
-    io.emit('chat message', `${name}님이 나갔습니다. 현재 접속유저수:${totalCount}`);
-  })
-});
-
-https.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`);
-});
